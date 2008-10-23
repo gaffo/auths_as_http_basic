3197dba2ecd8277ce4c9aa91caedae31310a3fa8
Mike Gaffney <mike.gaffney@asolutions.com>
2008-10-23 11:29:44 -0500

    updated to be cooler

--- lib/http_basic_auth.rb
+++ lib/http_basic_auth.rb
@@ -1,39 +1,42 @@
 module HttpBasicAuth
-  
+
   def self.included(base)
     base.extend ActMacro
   end
-  
+
   module ActMacro
-    def authenticates_via_http_basic class_to_check, username_field, password_field
+    def authenticates_via_http_basic(class_to_check, username_field, password_field, options = {})
       self.send(:include, HttpBasicAuth::InstanceMethods)
-      
+
       class_inheritable_reader :http_basic_auth__class_to_check
       write_inheritable_attribute :http_basic_auth__class_to_check, class_to_check
-      
-      
+
       class_inheritable_reader :http_basic_auth__username_field
       write_inheritable_attribute :http_basic_auth__username_field, username_field
-      
-      
+
       class_inheritable_reader :http_basic_auth__password_field
       write_inheritable_attribute :http_basic_auth__password_field, password_field
-      
+
+      class_inheritable_reader :http_basic_auth__conditions
+      write_inheritable_attribute(:http_basic_auth__conditions, (options[:conditions] || {}))
+
       before_filter :http_basic_auth_before_filter
     end
   end
-  
+
   module InstanceMethods
-    
+
     def http_basic_auth_before_filter
       authenticate_or_request_with_http_basic do |user_name, password|
         model_to_check = self.http_basic_auth__class_to_check.to_s.classify.constantize
-        user = model_to_check.find(:first, :conditions => {self.http_basic_auth__username_field => user_name,
-                                                           self.http_basic_auth__password_field => password})
+        conditions = {http_basic_auth__username_field => user_name,
+                      http_basic_auth__password_field => password}
+        conditions.merge!(http_basic_auth__conditions)
+        user = model_to_check.find(:first, :conditions => http_basic_auth__conditions)
         user.nil? ? false : true
       end
     end
-    
-  end 
-  
+
+  end
+
 end
